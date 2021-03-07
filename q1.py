9b2032837fb92060135e7a408d4d7340fb2f48be
Kaavya Subramanian <subramka@flip2.engr.oregonstate.edu>
2021-03-07 10:05:01 -0800

    Implemented printFizzBuzz which passes Test 2, specifications met

--- q1.py
+++ q1.py
@@ -1,11 +1,21 @@
 
 
 
+
 def printNumbers():
 	nums = [x for x in range(1,101)]
 	output = ""
+	for x in nums: output += str(x)
+	return output
+
+def printFizzBuzz():
+	nums = [x for x in range(1,101)]
+	output = ""
 	for x in nums:
-		output += str(x)
+		if x % 15 == 0: output += "FizzBuzz"
+		elif x % 5 == 0: output += "Buzz"
+		elif x % 3 == 0: output += "Fizz"
+		else: output += str(x)
 
 	
 	
--- q1_test.py
+++ q1_test.py
@@ -11,7 +11,7 @@ class TestCase(unittest.TestCase):
 	#Prints Fizz,Buzz, and FizzBuzz at the right times
 	def test2(self):
 		expectedString = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FizzBuzz4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FizzBuzz6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FizzBuzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FizzBuzz9192Fizz94BuzzFizz9798FizzBuzz"
-		self.assertEqual(q1.printNumbers(), expectedString)
+		self.assertEqual(q1.printFizzBuzz(), expectedString)
 
 if __name__ == '__main__':
 	unittest.main()	
